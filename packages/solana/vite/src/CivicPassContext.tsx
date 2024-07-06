fbeaa04b6173abac721f3b31815d55a2bc3a0019
dankelleher <dankelleher@yahoo.com>
2024-07-06 17:44:29 +0200

    Switch to useWallet for passing into the gateway provider

--- packages/solana/vite/src/CivicPassContext.tsx
+++ packages/solana/vite/src/CivicPassContext.tsx
@@ -1,12 +1,12 @@
 import { GatewayProvider } from "@civic/solana-gateway-react"
 import React, { FC, PropsWithChildren } from "react";
-import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+import {useConnection, useWallet} from "@solana/wallet-adapter-react";
 import {PublicKey} from "@solana/web3.js";
 
 export const UNIQUENESS_PASS = new PublicKey("uniqobk8oGh4XBLMqM68K8M2zNu3CdYX7q5go7whQiv")
 
 export const CivicPassProvider: FC<PropsWithChildren> = ({ children }) => {
-  const wallet = useAnchorWallet();
+  const wallet = useWallet();
   const { connection } = useConnection();
 
   return <GatewayProvider
