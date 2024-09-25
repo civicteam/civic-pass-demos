92e9f7b40e2502d5dd1308f07b64e90b53860e81
dankelleher <dankelleher@yahoo.com>
2024-09-25 08:28:37 +0200

    Path fixes

--- packages/evm/next/next.config.mjs
+++ packages/evm/next/next.config.mjs
@@ -1,8 +1,8 @@
 // Following the advice at https://github.com/vercel/vercel/discussions/5848 for handling rewrites/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = process.env.NODE_ENV === 'production' ? {
     basePath: '/next/evm',
     assetPrefix: "/next/evm",
     trailingSlash: false
-};
+} : {};
 
 export default nextConfig;
--- packages/solana/next/next.config.mjs
+++ packages/solana/next/next.config.mjs
@@ -1,8 +1,8 @@
 // Following the advice at https://github.com/vercel/vercel/discussions/5848 for handling rewrites/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = process.env.NODE_ENV === 'production' ? {
     basePath: '/next/solana',
     assetPrefix: "/next/solana",
     trailingSlash: false
-};
+} : {};
 
 export default nextConfig;
