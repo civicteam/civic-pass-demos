8e2ddc67447e73b8cb7ff5afe9df77f3f550f8a2
Flippie Scholtz <flippie@civic.com>
2024-12-12 19:50:17 +0200

    Fix auth URL and parameters in example script for looking up pass status using the partner API

--- packages/evm/exampleScripts/getPassStatusUsingAPI.ts
+++ packages/evm/exampleScripts/getPassStatusUsingAPI.ts
@@ -1,18 +1,21 @@
 // Get the state of a custom pass by calling the Civic Partner API with Oauth client credentials.
-// Usage: CLIENT_ID=<client-id> CLIENT_SECRET=<client-secret> bun run getPassStatusUsingAPI.ts <walletAddress> <chain>
+// Usage: CLIENT_ID=<client-id> CLIENT_SECRET=<client-secret> bun run getPassStatusUsingAPI.ts <walletAddress> <chainType> <chainNetwork>
+// For example, to look up a pass on Sepolia: 
+// CLIENT_ID=<client-id> CLIENT_SECRET=<client-secret> bun run getPassStatusUsingAPI.ts 0xaD067391A1C62e6fEafEe35a36fa520f9EDA48d9 ethereum sepolia
 // Full documentation: https://civicteam.github.io/openapi-docs
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const walletAddress = process.argv[2];
-const chain = process.argv[3];
+const chainType = process.argv[3];
+const chainNetwork = process.argv[4];
 
 // Authentication credentials for the Civic API
 const basicAuth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
 
 // The Civic endpoints used to authenticate and lookup the pass status
-const authUrl = "https://auth.civic.com/oauth/token";
-const lookupUrl = `https://api.civic.com/partner/pass/${chain}/${walletAddress}`;
+const authUrl = "https://auth0.civic.com/oauth/token";
+const lookupUrl = `https://api.civic.com/partner/pass/${chainType}/${chainNetwork}/${walletAddress}`;
 
 (async () => {
     // fetch the oauth client credentials token
